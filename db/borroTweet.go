d4c01a2a55a64dd6c3e3c0e537f80698724c6f48
AnthonyCM2K <anthonycriollo22@gmail.com>
2024-03-25 18:27:20 -0500

    chatgo-eliminarTweet

---
+++ db/borroTweet.go
@@ -0,0 +1,24 @@
+package db
+
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func BorroTweet(ID string, UserID string) error {
+	ctx := context.TODO()
+	bd := MongoCN.Database(DatabaseName)
+	col := bd.Collection("tweet")
+
+	objID, _ := primitive.ObjectIDFromHex(ID)
+
+	condicion := bson.M{
+		"_id":    objID,
+		"userid": UserID,
+	}
+
+	_, err := col.DeleteOne(ctx, condicion)
+	return err
+}
